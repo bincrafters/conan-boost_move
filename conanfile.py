f655ed265fb53e7b4852ebeb0e6b1956fd03718f
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:15 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostMoveConan(ConanFile):
     name = "Boost.Move"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/move"
+    url = "https://github.com/bincrafters/conan-boost-move"
+    source_url = "https://github.com/boostorg/move"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "move"
@@ -15,7 +15,7 @@ class BoostMoveConan(ConanFile):
                       
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+                 .format(self.version, self.source_url))
 
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
