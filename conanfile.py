9f456ba27d82e3eae30bf65f3b4be34f3bf44940
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:06 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,17 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostMoveConan(base.BoostBaseConan):
     name = "boost_move"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_move"
-    lib_short_names = ["move"]
-    header_only_libs = ["move"]
-    b2_requires = [
-        "boost_assert",
-        "boost_config",
-        "boost_core",
-        "boost_static_assert"
-    ]
+    version = "1.70.0"
